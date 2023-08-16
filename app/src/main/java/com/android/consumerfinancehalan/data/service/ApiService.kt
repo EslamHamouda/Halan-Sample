2da00d8d9758d600a289bbab92a3a4d90e93d5c7
Eslam hamouda ali <eslam.ee600@gmail.com>
2023-08-17 00:48:15 +0300

    NetworkModule setup

--- app/src/main/java/com/android/consumerfinancehalan/data/service/ApiService.kt
+++ app/src/main/java/com/android/consumerfinancehalan/data/service/ApiService.kt
@@ -1,4 +1,11 @@
 package com.android.consumerfinancehalan.data.service
 
+import com.android.consumerfinancehalan.data.model.BaseResponse
+import com.android.consumerfinancehalan.data.model.response.GetMerchantsResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ApiService {
+    @GET("vendors/{vendorId}/merchants")
+    suspend fun getMerchants(@Path("vendorId") vendorId:String): BaseResponse<GetMerchantsResponse>
 }
--- app/src/main/java/com/android/consumerfinancehalan/di/NetworkModule.kt
+++ app/src/main/java/com/android/consumerfinancehalan/di/NetworkModule.kt
@@ -1,10 +1,53 @@
 package com.android.consumerfinancehalan.di
 
+import com.android.consumerfinancehalan.data.service.ApiService
+import com.google.gson.GsonBuilder
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+    private const val BASE_URL = "https://api.halan.io/bff-mobile/vendor-Discovery/public/"
+    @Provides
+    @Singleton
+    @Named("RetrofitObject")
+    fun provideRetrofit(): Retrofit {
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        logging.setLevel(HttpLoggingInterceptor.Level.HEADERS)
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(Interceptor { chain: Interceptor.Chain ->
+            chain.proceed(
+                chain.request().newBuilder()
+                    .build()
+            )
+        })
+        httpClient.addInterceptor(logging)
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(httpClient.build())
+            .build()
+    }
+
+    @Provides
+    fun retrofitServices(
+        @Named("RetrofitObject")
+        retrofit: Retrofit
+    ): ApiService =
+        retrofit.create(ApiService::class.java)
 }
