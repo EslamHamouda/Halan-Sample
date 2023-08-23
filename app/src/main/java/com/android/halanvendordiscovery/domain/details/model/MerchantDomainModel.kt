4e66e9c89223068a30e98320d4fdd0a040e916c6
Eslam Hamouda Ali <69871727+EslamHamouda@users.noreply.github.com>
2023-08-23 18:34:22 +0300

    Update MerchantDomainModel.kt

--- app/src/main/java/com/android/halanvendordiscovery/domain/details/model/MerchantDomainModel.kt
+++ app/src/main/java/com/android/halanvendordiscovery/domain/details/model/MerchantDomainModel.kt
@@ -1,12 +1,12 @@
 package com.android.halanvendordiscovery.domain.details.model
 
 data class MerchantDomainModel(
-    val name            : String,
-    val arabicName      : String,
-    val address         : String,
-    val longitude       : Double,
-    val latitude        : Double,
-    val phoneNumber     : String,
-    val vendorName      : String,
-    val vendorArabicName: String,
+    val name            : String?,
+    val arabicName      : String?,
+    val address         : String?,
+    val longitude       : Double?,
+    val latitude        : Double?,
+    val phoneNumber     : String?,
+    val vendorName      : String?,
+    val vendorArabicName: String?,
 )
