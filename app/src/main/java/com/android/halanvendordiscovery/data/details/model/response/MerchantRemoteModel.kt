4daeba9cb5cdcb294d3dd2c7c0bb3030eea08ff9
eslam <eslam.ee600@gmail.com>
2023-08-24 01:08:31 +0300

    Refactor MerchantRemoteModel

--- app/src/main/java/com/android/halanvendordiscovery/data/details/model/response/MerchantRemoteModel.kt
+++ app/src/main/java/com/android/halanvendordiscovery/data/details/model/response/MerchantRemoteModel.kt
@@ -5,15 +5,20 @@ import com.google.gson.annotations.SerializedName
 data class MerchantRemoteModel(
     var id: String,
     var name: String?,
+    @SerializedName("arabic_name")
     var arabicName: String?,
+    @SerializedName("creation_date")
     var creationDate: Int?,
     var status: Int?,
     var address: String?,
     var governorate: String?,
     var longitude: Double?,
     var latitude: Double?,
+    @SerializedName("phone_number")
     var phoneNumber: String?,
+    @SerializedName("branch_head_name")
     var branchHeadName: String?,
+    @SerializedName("branch_head_phone")
     var branchHeadPhone: String?,
     var categories: String?,
     var merchantId: String?,
