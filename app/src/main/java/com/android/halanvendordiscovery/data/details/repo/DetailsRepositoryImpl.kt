d82a306f2a398933339068d275fe295cf0760a72
eslam <eslam.ee600@gmail.com>
2023-08-27 23:28:26 +0300

    remove unused imports

--- app/src/main/java/com/android/halanvendordiscovery/data/details/repo/DetailsRepositoryImpl.kt
+++ app/src/main/java/com/android/halanvendordiscovery/data/details/repo/DetailsRepositoryImpl.kt
@@ -1,17 +1,11 @@
 package com.android.halanvendordiscovery.data.details.repo
 
-import android.os.Looper
-import android.util.Log
 import com.android.halanvendordiscovery.data.details.mapper.MerchantRemoteDomainMapper
 import com.android.halanvendordiscovery.data.details.service.DetailsService
 import com.android.halanvendordiscovery.domain.details.model.MerchantDomainModel
 import com.android.halanvendordiscovery.domain.details.repo.DetailsRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.job
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.coroutines.CoroutineContext
 
 @Singleton
 class DetailsRepositoryImpl @Inject constructor(private val service: DetailsService):
