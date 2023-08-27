49c9068296c352436afbb0a2802385b752a656a5
eslam <eslam.ee600@gmail.com>
2023-08-27 16:06:51 +0300

    Update DetailsRepositoryImpl

--- app/src/main/java/com/android/halanvendordiscovery/data/details/repo/DetailsRepositoryImpl.kt
+++ app/src/main/java/com/android/halanvendordiscovery/data/details/repo/DetailsRepositoryImpl.kt
@@ -1,19 +1,24 @@
 package com.android.halanvendordiscovery.data.details.repo
 
+import android.os.Looper
+import android.util.Log
 import com.android.halanvendordiscovery.data.details.mapper.MerchantRemoteDomainMapper
 import com.android.halanvendordiscovery.data.details.service.DetailsService
+import com.android.halanvendordiscovery.domain.details.model.MerchantDomainModel
 import com.android.halanvendordiscovery.domain.details.repo.DetailsRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.job
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
 
 @Singleton
 class DetailsRepositoryImpl @Inject constructor(private val service: DetailsService):
     DetailsRepository {
-    override suspend fun getMerchants(vendorId: String) =
-        withContext(Dispatchers.IO) {
-                val merchantsResponse = service.getMerchants(vendorId)
-                merchantsResponse.data?.merchants?.map { dto -> MerchantRemoteDomainMapper.toDomain(dto) }.orEmpty()
-        }
+    override suspend fun getMerchants(vendorId: String): List<MerchantDomainModel> {
+        val merchantsResponse = service.getMerchants(vendorId)
+        return merchantsResponse.data?.merchants?.map { dto -> MerchantRemoteDomainMapper.toDomain(dto) }
+            .orEmpty()
+    }
 }
