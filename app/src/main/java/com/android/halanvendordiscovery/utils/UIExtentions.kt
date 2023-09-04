d5e4ff4dffe7470e5d2abb2ab2ac8fdfed4b9822
Omar Marei <omar.yahya.4556gmail.com>
2023-09-04 10:31:51 +0300

    Vendor presentation layer refactor

---
+++ app/src/main/java/com/android/halanvendordiscovery/utils/UIExtentions.kt
@@ -0,0 +1,24 @@
+package com.android.halanvendordiscovery.utils
+
+import android.app.Activity
+import android.view.View
+import android.widget.ProgressBar
+import com.google.android.material.snackbar.Snackbar
+
+fun ProgressBar.hideProgressBar() {
+    this.visibility = View.GONE
+}
+
+fun ProgressBar.showProgressBar() {
+    this.visibility = View.VISIBLE
+}
+
+fun showSnackBar(msg: String, activity: Activity) {
+    activity.view()?.let {
+        Snackbar.make(it, msg, Snackbar.LENGTH_LONG)
+            .show()
+    }
+}
+
+fun Activity.view(): View? =
+    findViewById(android.R.id.content)
